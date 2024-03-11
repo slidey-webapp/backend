99ab5f51d7b9d903fe07596d68df6f3d04af6e81
anh.nguyen <nguyenmyanh.160101@gmail.com>
2024-03-11 22:57:46 +0700

    change asset path for prod

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -1,9 +1,11 @@
 import multer from "multer";
 import path from "path";
 import crypto from "crypto";
-
+import { ENVIRONMENT } from "../config/contants";
+const isProduction = process.env.NODE_ENV === ENVIRONMENT.PRODUCTION;
+const __dirname = path.resolve() + isProduction ? "dist" : "src";
 const storage = multer.diskStorage({
-    destination: "./assets",
+    destination: __dirname + "/assets",
     filename: (req, file, cb) => {
         cb(
             null,
