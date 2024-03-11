112b57c629705d6d72794269067da581d56f39ef
anh.nguyen <nguyenmyanh.160101@gmail.com>
2024-03-11 23:20:19 +0700

    use MemoryStorage instead of DiskStorage

--- src/components/common/common.service.js
+++ src/components/common/common.service.js
@@ -9,6 +9,15 @@ export const uploadFile = async (file) => {
     };
 };
 
+export const uploadBuffer = async (buffer) => {
+    const result = await Cloudinary.uploadBuffer(buffer);
+    const { secure_url, public_id } = result;
+    return {
+        mediaURL: secure_url,
+        publicID: public_id,
+    };
+};
+
 export const destroyFile = async (publicID) => {
     const result = await Cloudinary.destroy(publicID);
     return result;
--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -1,18 +1,16 @@
 import multer from "multer";
 import path from "path";
 import crypto from "crypto";
-import { ENVIRONMENT } from "../config/contants";
-const isProduction = process.env.NODE_ENV === ENVIRONMENT.PRODUCTION;
-const __dirname = path.resolve() + isProduction ? "dist" : "src";
-const storage = multer.diskStorage({
-    destination: __dirname + "/assets",
-    filename: (req, file, cb) => {
-        cb(
-            null,
-            crypto.createHash("md5").update(Math.random().toString()).digest("hex") + path.extname(file.originalname)
-        );
-    },
-});
+// const storage = multer.diskStorage({
+//     destination: __dirname + "/assets",
+//     filename: (req, file, cb) => {
+//         cb(
+//             null,
+//             crypto.createHash("md5").update(Math.random().toString()).digest("hex") + path.extname(file.originalname)
+//         );
+//     },
+// });
+const storage = multer.memoryStorage();
 
 const upload = multer({
     storage: storage,
